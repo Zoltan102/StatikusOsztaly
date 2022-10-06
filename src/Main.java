fdf7adcd58d76594322342645ca020feb570cf2f
Zolizoli110 <73071177+Zolizoli110@users.noreply.github.com>
2022-10-06 10:03:41 +0200

    eddig jutottam

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Veletlen.velEgesz(5, 10));
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Veletlen.velKarakter('a', 'z'));
+        }
+        for (int i = 0; i < 10; i++) {
+            Random rnd = new Random();
+            System.out.println(Veletlen.velTeljesNev(rnd.nextBoolean()));
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Veletlen.velDatum(2021, 2021)) ;
+        }
     }
 }
---
+++ src/Veletlen.java
@@ -0,0 +1,133 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.time.Year.isLeap;
+
+public final class Veletlen {
+    private Veletlen() {
+    }
+
+    private static final Random rnd = new Random();
+    private static final List<String> vezNevek = feltolt("files/veznev.txt");
+    private static final List<String> ferfiKerNevek = feltolt("files/ferfikernev.txt");
+    private static final List<String> noiKerNevek = feltolt("files/noikernev.txt");
+
+    private static List<String> feltolt(String fileName) {
+        List<String> lista = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(new File(fileName));
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                lista.add(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(System.err);
+        }
+        return lista;
+    }
+
+    public static int velEgesz(int min, int max) {
+        return rnd.nextInt(max - min + 1) + min;
+    }
+
+    public static char velKarakter(char min, char max) {
+        return (char) velEgesz(min, max);
+    }
+
+    public static String velVezetekNev() {
+        return vezNevek.get(rnd.nextInt(vezNevek.size()));
+    }
+
+    /**
+     * Véletlen magyar keresztnév generálása
+     *
+     * @param nem A generált név neme. Férfi: true, Nő: false.
+     * @return A generált keresztnév
+     */
+    public static String velKeresztNev(boolean nem) {
+        String keresztNev;
+        if (nem) {
+            keresztNev = velFerfiKeresztNev();
+        } else {
+            keresztNev = velNoiKeresztNev();
+        }
+        return keresztNev;
+    }
+
+    private static String velFerfiKeresztNev() {
+        return ferfiKerNevek.get(rnd.nextInt(ferfiKerNevek.size()));
+    }
+
+    private static String velNoiKeresztNev() {
+        return noiKerNevek.get(rnd.nextInt(noiKerNevek.size()));
+    }
+
+    /**
+     * Véletlen magyar nevek generálása
+     *
+     * @param nem A generált név neme. Férfi: true, Nő: false.
+     * @return A generált név
+     */
+    public static String velTeljesNev(boolean nem) {
+        return velVezetekNev() + " " + velKeresztNev(nem);
+    }
+
+    public static String velDatum(int ev1, int ev2){
+        int year = velEv(ev1, ev2);
+        int month = velHonap();
+        int day = velNap(year, month);
+        return year + "-" + month + "-" + day;
+    }
+
+    private static int velEv(int min, int max){
+        return velEgesz(min, max);
+    }
+
+    private static int velHonap(){
+        return velEgesz(1, 12);
+    }
+
+    private static int velNap(int year, int month){
+        int day;
+            switch (month){
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    day = velEgesz(1, 31);
+                    break;
+                case 2:
+                    if (isLeap(year)){
+                        day = velEgesz(1, 29);
+                    }else {
+                        day = velEgesz(1, 28);
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+
+                    day = velEgesz(1, 30);
+                    break;
+                default:
+                    throw new RuntimeException("Nem egy valós hónap lett megadva!");
+            }
+        return day;
+    }
+
+    public static String velEmail(String nev){
+        String email;
+        nev = Normalizer.normalize(nev, Normalizer.Form.NFKD);
+        return email;
+    }
+}
